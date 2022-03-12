52ed3625c5170cbf9174ca9a512366a86c92a9a3
musyokamuasya <josephmuasya8@gmail.com>
2022-03-12 13:58:31 +0300

    Added comment on what promise and observable usecases

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -7,10 +7,12 @@ export class ProductsController {
   createProducts(): string {
     return 'This route creates products';
   }
+  //   Observable used to return a stream of data using rxjs
   @Get()
   findAllProducts(): Observable<any[]> {
     return of([]);
   }
+  //   promises uses to return one shot data
   @Get(':id')
   async findProductById(@Param() params): Promise<any[]> {
     console.log(params.id);
